9f48865b9783131dacffa13025f9a737a1ed8fd2
BuddhiLW <pedrogbranquinho@gmail.com>
2021-03-30 22:07:15 -0300

    Addition of Javascript to dynamically select CSS

--- resources/public/js/highlight-buys.js
+++ resources/public/js/highlight-buys.js
@@ -1,19 +1,47 @@
-var id11 = document.getElementById('id11')
-var id12 = document.getElementById('id12')
-var id13 = document.getElementById('id13')
-var id21 = document.getElementById('id21')
-var id22 = document.getElementById('id22')
-var id23 = document.getElementById('id23')
-var id31 = document.getElementById('id31')
-var id32 = document.getElementById('id32')
-var id33 = document.getElementById('id33')
+// var id11 = document.getElementById('id11')
+// var id12 = document.getElementById('id12')
+// var id13 = document.getElementById('id13')
+// var id21 = document.getElementById('id21')
+// var id22 = document.getElementById('id22')
+// var id23 = document.getElementById('id23')
+// var id31 = document.getElementById('id31')
+// var id32 = document.getElementById('id32')
+// var id33 = document.getElementById('id33')
 
 
-input.addEventListener('click', change_display())
+// input.addEventListener('click', change_display())
 
-function change_display(){
+// function change_display(){
     
-    footer.style.setProperty('--footer-color', input.value)
-}
+//     footer.style.setProperty('--footer-color', input.value)
+// }
 
 
+let selected = document.getElementsByClassName("selection");
+// let activeImages = document.getElementsByClassName("active");
+let buyImages = document.getElementsByClassName("buy");
+
+for (var i=0; i < selected.length; i++){
+// Variable responsible to count selected items in one of the rolls
+    selected[i].addEventListener("click", function(){
+	if (buyImages.length > 0){
+	    buyImages[0].classList.remove("buy");
+	    // this.classList.remove("active");
+	}
+	this.classList.add("buy");
+	// selection_index = selection_index + 1;
+    });
+};
+
+    // selected[i].addEventListener("mouseover", function(){
+    // 	if (activeImages.length > 0){
+    // 	    activeImages[0].classList.remove("active");	    
+	    
+    // 	} else if (this.classList.contains("buy")){
+    // 	    this.classList.remove("active");
+    // 	    this.classList.add("already-active");
+    // 	}
+    // 	this.classList.add("active");
+	
+	// document.getElementById("featured").src = this.src
+    // })
