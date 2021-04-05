4ae47463b2ec460cb58a38c24dd5713be27b8f20
BuddhiLW <pedrogbranquinho@gmail.com>
2021-04-05 12:22:06 -0300

    Moving this file back to working conditions Couldn't refactor with my current knowledge.

--- resources/public/js/highlight-all.js
+++ resources/public/js/highlight-all.js
@@ -3,33 +3,31 @@ let footerStyleSpan = document.querySelector(".foot span");
 let link = document.querySelector(".footer a");
 let combinedVar = buyImages.length + buyImagesDrink.length + buyImagesDessert.length;
 
-// foot style changer function.
-function styleChange() {
-    if (combinedVar === 3){
-	footerStyle[0].classList.add("order");
-	footerStyleSpan.innerHTML = "Fechar pedido";
-    }
+//add event listener in all elements, so to make the style-change independent of order of food selection.
+for (var m=0; m < selected.length; m++){
+    selected[m].addEventListener("click", function (){
+        if (combinedVar === 3){
+	    footerStyle[0].classList.add("order");
+	    footerStyleSpan.innerHTML = "Fechar pedido";
+        }
+    });
 };
-//add event listener in all elements, so to make style change be independent of order of layout selection.
+
+for (var m=0; m < selectedDrink.length; m++){
+    selectedDrink[m].addEventListener("click", function (){
+        if (combinedVar === 3){
+	    footerStyle[0].classList.add("order");
+	    footerStyleSpan.innerHTML = "Fechar pedido";
+        }
+    });
+};
+
 for (var m=0; m < selectedDessert.length; m++){
-    selectedDessert[m].addEventListener("click", styleChange);
-    selected[m].addEventListener("click", styleChange);
-    selectedDrink[m].addEventListener("click", styleChange);
+    selectedDessert[m].addEventListener("click", function (){
+        if (combinedVar === 3){
+	    footerStyle[0].classList.add("order");
+	    footerStyleSpan.innerHTML = "Fechar pedido";
+        }
+    });
 };
 
-
-// Ideas of funtional implementation
-// var funcOrder = function(selectVar) {
-//     let selectedVar = selectVar;
-//     [selectVar][i].addEventListener("click", styleChange());
-// };
-
-// var pSelectedDessert = funcOrder(selectedDessert);
-// var pSelectedDrink = funcOrder(selectedDrink);
-// var pSelected = funcOrder(selected);
-
-// for (var i = 0; i < selected.length; i++) {
-//     pSelectedDessert();
-//     pSelectedDrink();
-//     pSelected();
-// }
